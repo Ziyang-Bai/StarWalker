a96d3f38950d5166112bf1e23c91cf988fcfb27e
栢兹氧 <baiziyang2022@yeah.net>
2024-07-27 16:38:17 +0800

    移动了lib的位置 增加了config/config.json

--- starwalker/startup.py
+++ starwalker/startup.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import os
 from time import *
-
+import json
 def showWelcome():
     sw = root.winfo_screenwidth()
     sh = root.winfo_screenheight()
@@ -10,14 +10,20 @@ def showWelcome():
     y = (sh - 158) / 2
     root.geometry("475x158+%d+%d" % (x, y))
     root['bg'] = 'lightgray'
-
+    if os.path.exists(".//config//config.json"):
+        with open(".//config//config.json", "r") as f:
+            config = json.load(f)
+    else:
+        print("!ERROR! THE CONFIG.JSON IS MISSING!")
     if os.path.exists('.//lib//image//starwalker-welcome.png'):
         bm = PhotoImage(file='.//lib//image//starwalker-welcome.png')
         lb_welcomelogo = Label(root, image=bm, bg='black')
         lb_welcomelogo.bm = bm
         lb_welcomelogo.place(x=0, y=0)
-
-    lb_welcometext = Label(root, text='StarWalker - OpenSource Project - MPL-2.0 licence',
+    else:
+        print("!ERROR! THE STARWALKER-WELCOME.PNG IS MISSING!")
+    lb_welcometext_text = config['name'] + " " + config['version'] + " " + config['snapshout']
+    lb_welcometext = Label(root, text=lb_welcometext_text,
                            fg='black', bg='lightgray', font=('Courier', 10))
     lb_welcometext.place(x=0, y=143, width=475, height=15)
 
